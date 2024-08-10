c3684beb7e835c1a81a6b6e7f986c7da91782635
toro_ponz <toro-ponz@users.noreply.github.com>
2024-08-11 01:43:52 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    application
+}
+
+group = "io.toroponz"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
+}
